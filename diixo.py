79b7d5b24b778f3a85cf0aa46306494a82508d0a
diixo <anatoliy.podoba@dxc.com>
2025-06-01 00:10:37 +0300

    updated-123

--- diixo.py
+++ diixo.py
@@ -35,7 +35,8 @@ diixo = [
     'web', 'assembl', 'develop', 'work', 'design', 'thought',
     'round', 'around', 'flow', 'sheet', 'time', 'shop', 'group', 'local', 'ground', 'stack', 'sketch', 'sentiment', 'life',
     'with', 'through', 'rout', 'stand', 'shout', 'routine', 'scout', 'roll', 'perform', 'bound', 'look', 'scroll', 'standard', 'break',
-    #'about', 'out', #after, often, turn, under, point, under, upper, back, low
+    #'about', 'out', #after, often, turn, under, point, under, upper, low, call, fall, non, none, nope
+    'back', 'switch', 'throw', 'trac', 'track', 'tract', 'author', 'determin', 'detect', 'desir', #distract, distort,
     ]
 
 #print(sorted(diixo))
