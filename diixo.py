12603723b7c0e06a2926070f42803adeb1fa8e8a
diixo <anatoliy.podoba@dxc.com>
2025-05-31 19:32:04 +0300

    updated

--- diixo.py
+++ diixo.py
@@ -25,7 +25,7 @@ diixo = [
     'diagnos', 'dis', 'discuss', 'miss', 'dispens', 'dispers', 'dispos', 'regard', 'stress', 'gross', 'expos', 'express', 'guess', 'possib',
     'spect', 'licens', 'los', 'mess', 'opposs', 'pars', 'press', 'process', 'profess', 'progress', 'regress', 'propos', 'purpos', 'vers', 'sens',
     'sing', 'stress',
-    'key', 'word',
+    'key', 'word', 'form', 'base',
     ]
 
 print(sorted(diixo))
