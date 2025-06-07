5cff464aad76e72f467e6cbd323c2de23964bcc4
diixo <anatoliy.podoba@dxc.com>
2025-06-07 07:22:08 +0300

    2880

--- diixo.py
+++ diixo.py
@@ -224,6 +224,8 @@ diixo = [
     'revisit', 'scar', 'care', 'carr', 'architect', 'archiv', 'arbit', 'arch', 'margin', 'marginal', 'area', 'scatter', 'hedron',
     'vanish', 'car', 'arduino', 'api', 'valu', 'vector', 'eight', 'for', 'app', 'apposit', 'entrap', 'dream', 'dron', 'drill',
     'octa', 'dub', 'dress', 'driv', 'drink', 'talk', 'tall', 'drag', 'engross', 'entitl', 'eradicate', 'litera', 'obliterat',
+    'plac', 'display', 'plain', 'planar', 'plan', 'plane', 'planet', 'complacen', 'bulk', 'buch',
+    #'complacen', 'emplace',
     # er?, gence, gance, que, date, que, case, marathon
     # add 'summar', 'summat', substance substantial
     # late, fire light wield win ful wise prov api
