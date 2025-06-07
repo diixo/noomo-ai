8badcfb093f6ef860aab1085fa8a63ee44d0e893
diixo <anatoliy.podoba@dxc.com>
2025-06-07 05:10:38 +0300

    2820

--- diixo.py
+++ diixo.py
@@ -219,9 +219,9 @@ diixo = [
     'overlap', 'recap', 'tap', 'trap', 'trash', 'ball', 'tangent', 'apostrophe', 'anniversary', 'chunk', 'destruct',
     'accord', 'head', 'ache', 'man', 'seo', 'skip', 'impos', 'detach', 'mandat', 'impossibl', 'roman', 'accomplish',
     'reproach', 'settl', 'setup', 'woman', 'upset', 'broach', 'destroy', 'destin', 'delta', 'hub', 'act', 'delv', 'acronym',
-    'arti', 'ficial', 'art', 'artist',
-    # thunk, quarter, thwart, quarrel, quaternion, earth, carv, cart, arrogant, carr?, er?, gence, gance, que, bird
-    # date
+    'arti', 'ficial', 'art', 'artist', 'quarter', 'earth', 'carv', 'cart', 'thwart', 'arrogant', 'cascad',
+    'quarrel', 'quaternion', 'quant',
+    # thunk, carr?, er?, gence, gance, que, bird, date
     # add 'summar', 'summat', substance substantial
     # late, fire light wield win ful wise prov api
     #set, heal, repo, side, rand, mov, def, nom-nomo, uous, duc
