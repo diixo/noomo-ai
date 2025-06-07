2a5b9205df7768f0b3fc6c74f07dac6727225a67
diixo <anatoliy.podoba@dxc.com>
2025-06-07 06:25:47 +0300

    2860

--- diixo.py
+++ diixo.py
@@ -221,8 +221,9 @@ diixo = [
     'reproach', 'settl', 'setup', 'woman', 'upset', 'broach', 'destroy', 'destin', 'delta', 'hub', 'act', 'delv', 'acronym',
     'arti', 'ficial', 'art', 'artist', 'quarter', 'earth', 'carv', 'cart', 'thwart', 'arrogant', 'cascad', 'apart', 'revis',
     'quarrel', 'quaternion', 'quant', 'queer', 'queen', 'thunk', 'nail', 'apache', 'bird', 'reprehen', 'apartment',
-    'revisit', 'scar', 'care', 'carr', 'architect', 'archiv', 'arbit', 'arch', 'margin', 'marginal', 'area', 'scatter',
-    'vanish', 'car', 'arduino', 'api', 'valu', 'vector', 'eight', 'for',
+    'revisit', 'scar', 'care', 'carr', 'architect', 'archiv', 'arbit', 'arch', 'margin', 'marginal', 'area', 'scatter', 'hedron',
+    'vanish', 'car', 'arduino', 'api', 'valu', 'vector', 'eight', 'for', 'app', 'apposit', 'entrap', 'dream', 'dron', 'drill',
+    'octa', 'dub', 'dress',
     # er?, gence, gance, que, date, que, case, marathon
     # add 'summar', 'summat', substance substantial
     # late, fire light wield win ful wise prov api
