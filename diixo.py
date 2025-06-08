9d84dadacfb731d128f380a3506a7e685c92bf4c
diixo <anatoliy.podoba@dxc.com>
2025-06-08 08:56:19 +0300

    3170

--- diixo.py
+++ diixo.py
@@ -245,7 +245,8 @@ diixo = [
     'obstruct', 'old', 'omit', 'hold', 'gold', 'thres', 'got', 'vigour', 'django', 'rigorous', 'logit', 'logistic', 'gorgeous', 'poll',
     'during', 'duration', 'durab', 'notat', 'cur', 'curl', 'light', 'male', 'female', 'equaliz', 'linguist', 'compel', 'sill',
     'mak', 'made', 'get', 'hav', 'has', 'haskell', 'captcha', 'hast', 'hassl', 'haunt', 'hunt', 'hurr', 'lodg', 'loose', 'lov', 'loud',
-    'low', 'luxur', 'mama', 'marr', 'indign', #'meet', 'meditat', 'merit', 'messag',
+    'low', 'luxur', 'mama', 'marr', 'indign', 'meet', 'meditat', 'merit', 'messag', 'demodulat', 'late', 'option', 'outage', 'overrid',
+    'saturat',
     #'ling', #'near', #'lin', # 'gap', 'eag', 'low', 'abat', 'tim' ring
     #'out', 'our', 'mon', lin lif liv pace
     #'emplace',
