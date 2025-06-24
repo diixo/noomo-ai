7a56758fef0bc7e784b799642b28f3a4da129143
diixo <anatolii.podoba@gmail.com>
2025-06-24 08:08:51 +0300

    3840

--- diixo.py
+++ diixo.py
@@ -292,8 +292,9 @@ diixo = [
     'arduous', 'samsung', 'ops', 'pop', 'dev', 'devis', 'deviat', 'detract', 'devoid', 'opaque', 'opaci', 'devalu', 'devastat', 'azure',
     'averag', 'avatar', 'avert', 'autumn', 'associat', 'auxiliar', 'aton', 'atlas', 'boss', 'jira', 'jinja', 'add', 'addit', 'adept',
     'jott', 'jut', 'kotlin', 'kubernet', 'jdo', 'jest', 'dom', 'majest', 'jdk', 'jvm', 'jre', 'jwt', 'dbc', 'jpg', 'jpeg',
-    'legac', 'legib', 'discount', 'discourag', 'discreet', 'led', 'amo', 'assail', 'crumbl', 'leap', 'baffl', 'flav',
-    'flew', 'flee', 'fled', 'fling', 'flirt', 'fly', 'focal', 'flutter', 'fli', 'afflict',
+    'legac', 'legib', 'discount', 'discourag', 'discreet', 'led', 'amo', 'assail', 'crumbl', 'leap', 'baffl', 'flav', 'fixat',
+    'flew', 'flee', 'fled', 'fling', 'flirt', 'fly', 'focal', 'flutter', 'fli', 'afflict', 'affix', 'floor', 'flopp', 'five', 'flask',
+    'alternat', 'agent', 'agglomerat', 'agenc',
     #'sit', 'set', worfklow, disc, at
     # rat, tri, son, perma-link neura-link euclid tiny tip bi- , hit,
     #'ling', #'lin', # 'gap', 'eag', 'tim' ring,
