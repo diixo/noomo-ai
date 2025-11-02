247c2700e85c562d3d35a1cacdff52862c484e1f
diixo <anatolii.podoba@gmail.com>
2025-11-02 03:48:28 +0200

    4216

--- diixo.py
+++ diixo.py
@@ -337,8 +337,8 @@ diixo = [
     'original', 'originat', 'per', 'nab', 'lua', 'ltd', 'penur', 'iterat', 'introduc', 'perennial', 'perempt', 'imperat', 'imperativ',
     'defy', 'defi', 'deny', 'deni', 'dum', 'din', 'dim', 'tid', 'delid', 'ive', 'prod', 'wed', 'irrit', 'administrat', 'avow', 'sum',
     'ible', 'evit', 'commod', 'iron', 'irk', 'ibly', 'ities', 'ocul', 'cult', 'ence', 'pet', 'ize', 'iza', 'ilit', 'scept', 'sullen',
-
-    #'ous', 'ance', 'ble', 'ency', 'ial', 'ising',
+    'cat', 'cater', 'catal', 'catapult', 'ton', 'tonight', 'anxiet', 'embrocat', 'embrac', 'antonym', 'mal', 'tomorrow', 'tongue',
+    #'ous', 'ance', 'ble', 'ency', 'ial', 'ising', 'ent', 'ation', 'izing', 'maxim', 'minim', 'phob',
     #'sit', worfklow, retargatable, disc, at, ace, lumin++, croach++, cry++, in, out, servicedesk, autodesk
     # rat, tri, son, perma-link neura-link euclid tip bi- , hit,
     #'ling', #'lin', # 'gap', 'eag', 'tim' ring,
